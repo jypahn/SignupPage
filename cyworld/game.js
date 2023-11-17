62de92b0a0ddc4a01920cc31c9c912f54c4b3937
airro <jypahn@naver.com>
2023-11-17 22:40:26 +0900

    싸이월드 웹 5일차

--- cyworld/game.js
+++ cyworld/game.js
@@ -13,3 +13,21 @@ let start = () =>{
 
     }
 }
+
+const createnum = () => {
+    let lotto = [];
+    while(lotto.length < 6){
+    let num = Math.floor(Math.random()*45+1)
+    if(lotto.indexOf(num) == -1){
+        lotto.push(num)
+      }
+    }
+    lotto.sort((a,b)=>a-b);
+
+    document.getElementById("lotto__num1").innerText = lotto[0]
+    document.getElementById("lotto__num2").innerText = lotto[1]
+    document.getElementById("lotto__num3").innerText = lotto[2]
+    document.getElementById("lotto__num4").innerText = lotto[3]
+    document.getElementById("lotto__num5").innerText = lotto[4]
+    document.getElementById("lotto__num6").innerText = lotto[5]
+}
